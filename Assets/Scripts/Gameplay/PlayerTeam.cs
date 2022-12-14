7fb3c75ab4424d9a6d2c595f63a845c3baa508d2
jagerwil <vlad1.0@list.ru>
2022-12-14 15:32:31 +0300

    Removed placeholder logic from player team

--- Assets/Scripts/Gameplay/PlayerTeam.cs
+++ Assets/Scripts/Gameplay/PlayerTeam.cs
@@ -1,27 +1,8 @@
-using System.Collections;
-using UnityEngine;
 
 namespace TestGwentGame.Gameplay {
     public sealed class PlayerTeam : BaseTeam {
         public override void StartTeamTurn() {
             base.StartTeamTurn();
-            Debug.Log("<color=green>It's player turn!</color>");
-            //Allow player to use his cards or something
-            StartCoroutine(StartTurnCoro());
-        }
-
-        IEnumerator StartTurnCoro() {
-            //Code for testing
-            foreach (var pawn in _pawns) {
-                var targets = GetPawnTargets(pawn);
-                var targetIndex = Random.Range(0, targets.Count);
-
-                var target = targets[targetIndex];
-
-                yield return new WaitForSeconds(1f);
-                Debug.Log($"Use effect of pawn {pawn.DebugName} on target {target.DebugName}");
-                UsePawn(pawn, target);
-            }
         }
     }
 }
