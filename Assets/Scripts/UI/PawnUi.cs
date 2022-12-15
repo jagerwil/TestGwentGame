be1199ced12bf094e74913edb22f9411ede0fd6f
jagerwil <vlad1.0@list.ru>
2022-12-15 03:31:13 +0300

    Setup all status effects, poison icon now appears when poison is applied to the pawn

--- Assets/Scripts/UI/PawnUi.cs
+++ Assets/Scripts/UI/PawnUi.cs
@@ -13,24 +13,31 @@ namespace TestGwentGame.UI {
         public void Setup(Pawn pawn) {
             _pawn = pawn;
             SetupPawnHealth();
+            SetupPoisonIcon();
             _actionUi.Setup(pawn);
         }
 
         public void Refresh() {
             SetupPawnHealth();
+            SetupPoisonIcon();
+
             _actionUi.Refresh();
         }
 
         void OnEnable() {
+            EventManager.onPawnStatusEffectStarted.AddListener(OnStatusEffectStarted);
+            EventManager.onPawnStatusEffectEnded.AddListener(OnStatusEffectEnded);
             EventManager.onPawnHealthChanged.AddListener(OnHealthChanged);
         }
 
         void OnDisable() {
+            EventManager.onPawnStatusEffectStarted.RemoveListener(OnStatusEffectStarted);
+            EventManager.onPawnStatusEffectEnded.RemoveListener(OnStatusEffectEnded);
             EventManager.onPawnHealthChanged.RemoveListener(OnHealthChanged);
         }
 
         void SetupPawnHealth() {
-            if (!_pawn) {
+            if ( !_pawn ) {
                 return;
             }
 
@@ -43,6 +50,27 @@ namespace TestGwentGame.UI {
             var healthInfo = _pawn.Health;
             _healthUi.SetupHealth(healthInfo.Health);
             _extraHealthUi.SetupHealth(healthInfo.ExtraHealth);
+        }
+
+        void SetupPoisonIcon() {
+            var hasPoison = _pawn.StatusEffects.HaveStatusEffect(StatusEffectType.Poison);
+            _poisonIcon.SetActive(hasPoison);
+        }
+
+        void OnStatusEffectStarted(Pawn pawn, StatusEffectType type) {
+            if ( pawn != _pawn || type != StatusEffectType.Poison ) {
+                return;
+            }
+
+            _poisonIcon.SetActive(true);
+        }
+
+        void OnStatusEffectEnded(Pawn pawn, StatusEffectType type) {
+            if ( pawn != _pawn || type != StatusEffectType.Poison ) {
+                return;
+            }
+
+            _poisonIcon.SetActive(false);
         }
 
         void OnHealthChanged(Pawn pawn) {
